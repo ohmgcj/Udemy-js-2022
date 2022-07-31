e9ae1bd51b82996bde7a3d5b9097188a25ce6b67
j4c2 <j4c.2@hotmail.com>
2022-07-30 22:51:45 -0300

    Aula 135 - Foreach02

--- Curso JS/EXERCUCIOS-JS/array/foreache2.js
+++ Curso JS/EXERCUCIOS-JS/array/foreache2.js
@@ -1,11 +1,12 @@
+Array.prototype.forEach2 = function(callback) {
+    for (let i=0; i<this.length; i++){
+        callback(this[i], i, this)
+    }
+}
+
 const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
 
 // Percorre cada indice realizando uma ação
 aprovados.forEach(function(nome, indice) {
     console.log(`${indice + 1}) ${nome}`)
 }) 
-
-aprovados.forEach(nome => console.log(nome))
-
-const exibirAprovados = aprovado => console.log(aprovado)
-aprovados.forEach(exibirAprovados)
