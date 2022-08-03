c535a30aa21297f9dbb87838ae6951dde425e7c7
j4c2 <j4c.2@hotmail.com>
2022-08-02 23:41:06 -0300

    Aula 145 - Concat

--- Curso JS/EXERCUCIOS-JS/array/imperativoVsDeclarativo.js
+++ Curso JS/EXERCUCIOS-JS/array/imperativoVsDeclarativo.js
@@ -16,3 +16,5 @@ const getNota = aluno => aluno.nota
 const soma = (total, atual) => total + atual
 const total2 = alunos.map(getNota).reduce(soma)
 console.log(total2 / alunos.length)
+
+// selecet codigo, nome, from cliente where ativo = 1
