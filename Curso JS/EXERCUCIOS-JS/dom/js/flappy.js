60178945bd301944585b15bacbe892664e23bbfc
j4c2 <j4c.2@hotmail.com>
2022-11-30 03:12:46 -0300

    Aula 299 - Flappy #06

--- Curso JS/EXERCUCIOS-JS/dom/js/flappy.js
+++ Curso JS/EXERCUCIOS-JS/dom/js/flappy.js
@@ -126,6 +126,27 @@ setInterval(() => {
 }, 20)
 */
 
+function estaoSobrepostos(elementoA, elementoB) {
+    const a = elementoA.getBoundingClientRect()
+    const b = elementoB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left
+    const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top
+    return horizontal && vertical
+}
+
+function colidiu(passaro,barreiras) {
+    let colidiu = false
+    barreiras.pares.forEach(parDeBarreiras => {
+        if (!colidiu) {
+            const superior = parDeBarreiras.superior.elemento
+            const inferior = parDeBarreiras.inferior.elemento
+            colidiu = estaoSobrepostos(passaro.elemento, superior) || estaoSobrepostos(passaro.elemento, inferior)
+        }
+    })
+    return colidiu
+}
+
 function FlappyBird() {
     let pontos = 0
     const areaDoJogo = document.querySelector('[wm-flappy]')
@@ -142,9 +163,13 @@ function FlappyBird() {
 
     this.start = () => {
         // loop do jogo
-        const temporizado = setInterval(() => {
+        const temporizador = setInterval(() => {
             barreiras.animar()
             passaro.animar()
+
+            if (colidiu(passaro, barreiras)) {
+                clearInterval(temporizador)
+            }
         }, 20)
     }
 }
