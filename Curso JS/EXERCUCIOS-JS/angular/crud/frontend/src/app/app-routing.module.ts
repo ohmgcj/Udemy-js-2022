5d05341c3ad084d7dd5a9cb81673c9b5d45060ca
Caio Alves <j4c.2@hotmail.com>
2023-06-29 13:25:29 -0300

    Aula 533

--- Curso JS/EXERCUCIOS-JS/angular/crud/frontend/src/app/app-routing.module.ts
+++ Curso JS/EXERCUCIOS-JS/angular/crud/frontend/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component'
 import { ProductCrudComponent } from './views/product-crud/product-crud.component'
-
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 const routes: Routes = [{
   path: "",
@@ -11,6 +11,9 @@ const routes: Routes = [{
 },{
   path: "products",
   component: ProductCrudComponent
+},{
+  path: "products/create",
+  component: ProductCreateComponent
 }];
 
 @NgModule({
